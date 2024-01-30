531437d853dabac0b284063987717233890d1964
NandeeshNarayan <nandigowda478@gmail.com>
2024-01-30 17:53:48 +0530

    first commit

---
+++ test-output-thread/report.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([{"id":"bf0ea134-3cfa-4434-b4d2-68f0679312fe","feature":"Register multiple users on the Rediff account Page","scenario":"Test rediff Register Account Page","start":1706615504473,"group":1,"content":"","tags":"","end":1706615524830,"className":"passed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
+});
